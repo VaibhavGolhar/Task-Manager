bc4cc9bd5f808fbf9803ae064b64b7eae3a48657
Vaibhav Golhar <vaibhavgolhar0015@gmail.com>
2025-04-19 23:56:24 +0530

    controller added to fetch employees

---
+++ task-manager-backend/controllers/employeeFetchController.js
@@ -0,0 +1,25 @@
+const asyncHandler = require("express-async-handler");
+const sql = require('mssql');
+const pool = require('../config/dbConnection');
+
+const fetchEmployees = asyncHandler(async (req, res) => {
+    try {
+      console.log('Fetching employees...');
+      const result = await pool.query('SELECT EmployeeId, EmployeeName FROM dbo.EmployeeMaster;');
+      
+      // Get the actual rows from the result object
+      const rows = result.recordset;
+      
+      const employees = rows.map((emp) => ({
+        id: emp.EmployeeId,
+        name: emp.EmployeeName
+      }));
+      console.log('Employees fetched successfully:', employees);
+      res.status(200).json(employees);
+    } catch (error) {
+      console.error('Error fetching employees:', error);
+      res.status(500).json({ error: 'Failed to fetch employees' });
+    }
+  });
+
+  module.exports = { fetchEmployees };
--- task-manager-backend/routes/employeesRoutes.js
+++ task-manager-backend/routes/employeesRoutes.js
@@ -1,25 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const pool = require('../config/dbConnection');
+const { fetchEmployees } = require('../controllers/employeeFetchController');
 
-router.get('/', async (req, res) => {
-  try {
-    console.log('Fetching employees...');
-    const result = await pool.query('SELECT EmployeeId, EmployeeName FROM dbo.EmployeeMaster;');
-    
-    // Get the actual rows from the result object
-    const rows = result.recordset;
-    
-    const employees = rows.map((emp) => ({
-      id: emp.EmployeeId,
-      name: emp.EmployeeName
-    }));
-    console.log('Employees fetched successfully:', employees);
-    res.status(200).json(employees);
-  } catch (error) {
-    console.error('Error fetching employees:', error);
-    res.status(500).json({ error: 'Failed to fetch employees' });
-  }
-});
+router.route('/').get(fetchEmployees);
 
 module.exports = router;
